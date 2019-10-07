3ed0e22cb1c315875b014358936586520cf84280
vagrant <vagrant@homestead>
2019-10-07 01:15:31 +0000

    delted unnecessary assets and chnage the length of phone field to 100 in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('address')->nullable();
-            $table->string('Phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('Phone', 100)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
